20369311ed14fd6a75306357818d5eaa3dfa6c3a
Matt <mhughes.ipod@gmail.com>
2019-05-17 19:51:13 -0700

    new

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
         <h1>Matthew Hughes</h1>
         <h3>(925) 963 3487</h3>
         <a href="mailto:mhughes.lhCA@gmail.com?Subject=Hello%20there!" target="_top"><h3>mhughes.lhCA@gmail.com</h3></a>
-        <br/>
+<br/>
         <br/>
         <p>Feel free to contact me in regards to website building, code analysis, or other questions.</p>
     </div>
