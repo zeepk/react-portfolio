a53b68b81cb3bddf230b90066b95f8dc2363d226
Matt Hughes <matthew.l.hughes@kp.org>
2020-01-17 11:25:27 -0800

    reformatted projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Project from "./project";
-import ProjectReverse from "./projectReverse";
+// import ProjectReverse from "./projectReverse";
 import "./projects.css";
 
 export default function Projects() {
@@ -17,7 +17,7 @@ export default function Projects() {
         <div className="projects-grid-item">
           <Project
             title="Woodcut"
-            image_source={require("./woodcut.PNG")}
+            image_source={require("../../images/woodcut.PNG")}
             image_path="https://woodcut.info/rs3/zee_pk"
             description="This is a project I currently maintain for a video game. It is a high score checking web 
         application that checks user entered data against multiple API's. React Router is used for navigation across many pages. The page knows when a username searched for does not exist and 
@@ -30,9 +30,9 @@ export default function Projects() {
 
         <div></div>
         <div className="projects-grid-item">
-          <ProjectReverse
+          <Project
             title="Tech Support Site"
-            image_source={require("./superpcconsulting.png")}
+            image_source={require("../../images/superpc.png")}
             image_path="https://techsupportsite.netlify.com/"
             description="This is a template site I created to serve as a foundation for a tech support company website."
             tech="React, JavaScript, HTML, CSS"
@@ -45,7 +45,7 @@ export default function Projects() {
         <div className="projects-grid-item">
           <Project
             title="Ascii Checker"
-            image_source={require("./ascii.PNG")}
+            image_source={require("../../images/ascii.PNG")}
             image_path="https://asciichecker.netlify.com/"
             description="A site I created to quickly display the Ascii value of any character entered on the keyboard. The function reads the keypress event from the keyboard and changes the site to display the corresponding Ascii value."
             tech="JavaScript, HTML, CSS"
