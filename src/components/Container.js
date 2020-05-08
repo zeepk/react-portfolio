1c2e99fc4205e0755fabe0a889f383f7f98f8939
Matt <matt@matthughes.dev>
2020-05-07 21:59:01 -0700

    form

--- src/components/Container.js
+++ src/components/Container.js
@@ -88,7 +88,8 @@ class Container extends Component {
 						</Grid>
 					</Grid>
 				</div>
-				<form name="contact" method="POST" data-netlify="true">
+				<form name="contact" method="post">
+					<input type="hidden" name="form-name" value="contact" />
 					<p>
 						<label>
 							Your Name: <input type="text" name="name" />
@@ -97,15 +98,6 @@ class Container extends Component {
 					<p>
 						<label>
 							Your Email: <input type="email" name="email" />
-						</label>
-					</p>
-					<p>
-						<label>
-							Your Current Role:
-							<select name="role[]" multiple>
-								<option value="leader">Leader</option>
-								<option value="follower">Follower</option>
-							</select>
 						</label>
 					</p>
 					<p>
