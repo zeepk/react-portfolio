6917501a7a4707e780515adc83a540837975279e
unknown <mhughes.ipod@gmail.com>
2020-05-09 13:51:45 -0700

    preview test

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,69 @@
 import React, { Component } from 'react';
 import '../styles/Form.css';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 class Form extends Component {
 	render() {
 		return (
 			<div className="form-container">
-				<form name="contact" method="post">
-					<input type="hidden" name="form-name" value="contact" />
-					<p>
-						<label>
-							Your Name: <input type="text" name="name" />
-						</label>
-					</p>
-					<p>
-						<label>
-							Your Email: <input type="email" name="email" />
-						</label>
-					</p>
-					<p>
-						<label>
-							Message: <textarea name="message"></textarea>
-						</label>
-					</p>
-					<p>
-						<button type="submit">Send</button>
-					</p>
-				</form>
+				<Card className="card-container">
+					<CardContent>
+						<form name="contact" method="post">
+							<input type="hidden" name="form-name" value="contact" />
+							<p>
+								<label>
+									Your Name: <input type="text" name="name" />
+								</label>
+							</p>
+							<p>
+								<label>
+									Your Email: <input type="email" name="email" />
+								</label>
+							</p>
+							<p>
+								<label>
+									Message: <textarea name="message"></textarea>
+								</label>
+							</p>
+							<p>
+								{/* <button type="submit">Send</button> */}
+								<button type="button">submit</button>
+							</p>
+						</form>
+					</CardContent>
+					{/* <CardActions>
+						<Button size="small">Learn More</Button>
+					</CardActions> */}
+				</Card>
 			</div>
+			//     <Card >
+			// 	<CardContent>
+			// 	<form name="contact" method="post">
+			// 		<input type="hidden" name="form-name" value="contact" />
+			// 		<p>
+			// 			<label>
+			// 				Your Name: <input type="text" name="name" />
+			// 			</label>
+			// 		</p>
+			// 		<p>
+			// 			<label>
+			// 				Your Email: <input type="email" name="email" />
+			// 			</label>
+			// 		</p>
+			// 		<p>
+			// 			<label>
+			// 				Message: <textarea name="message"></textarea>
+			// 			</label>
+			// 		</p>
+			// 		<p>
+			// 			<button type="submit">Send</button>
+			// 		</p>
+			// 	</form>
+			// 	</CardContent>
+			// 	<CardActions>
+			// 	  <Button size="small">Learn More</Button>
+			// 	</CardActions>
+			//   </Card>
 		);
 	}
 }
