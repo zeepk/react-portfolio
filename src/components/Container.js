8bf8a2e5e1c51384ee20102604f334f478cb30c0
unknown <mhughes.ipod@gmail.com>
2020-05-08 20:16:06 -0700

    clouds

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import '../styles/Container.css';
-import Project from './Project';
-import Grid from '@material-ui/core/Grid';
-import EMT from '../images/podcast.png';
-import SuperPC from '../images/superpc.png';
-import Wildlife from '../images/wildlife.png';
-import Woodcut from '../images/woodcut2.png';
-import Arma from '../images/arma.png';
-import Payments from '../images/payments.png';
-
 import About from './About';
 import Form from './Form';
+import ProjectGrid from './ProjectGrid';
+
 class Container extends Component {
 	constructor(props) {
 		super(props);
@@ -37,64 +30,17 @@ class Container extends Component {
 	render() {
 		return (
 			<div className="container">
+				<div class="background-container"></div>
+				<div class="clouds"></div>
 				{/* <Header /> */}
 				<About />
 				<svg className="blinker" style={{ opacity: this.state.arrow }}>
 					<line className="line1" x1="40%" y1="30%" x2="50%" y2="50%" />
 					<line className="line1" x1="50%" y1="50%" x2="60%" y2="30%" />
 				</svg>
-				<div className="projects-container">
-					<Grid container spacing={3}>
-						<Grid className="project-grid-item" item xs={12} sm={6}>
-							<Project
-								title="Game Server Homepage"
-								body="Site for users to see news and updates for a video game server. Owner can update via Django Admin."
-								filename={Arma}
-								link="https://arma-homepage-demo.herokuapp.com/"
-							/>
-						</Grid>
-						<Grid className="project-grid-item" item xs={12} sm={6}>
-							<Project
-								title="Animal Crossing Wildlife"
-								body="Front End site for gamers to track achievements and see wildlife information. Saves progress using local storage."
-								filename={Wildlife}
-								link="https://www.acwildlife.com"
-							/>
-						</Grid>
-						<Grid className="project-grid-item" item xs={12} sm={6}>
-							<Project
-								title="Podcast Landing Page"
-								body="Landing site for information regarding an EMT podcast. Uses SVG for the background animation."
-								filename={EMT}
-								link="https://emtcast2.netlify.app/"
-							/>
-						</Grid>
-						<Grid className="project-grid-item" item xs={12} sm={6}>
-							<Project
-								title="Consultancy Support Site"
-								body="Business home page for a sample tech support company. Uses animations and parallax imagery for the background."
-								filename={SuperPC}
-								link="https://techsupportsite.netlify.app/"
-							/>
-						</Grid>
-						<Grid className="project-grid-item" item xs={12} sm={6}>
-							<Project
-								title="Payment Manager Web App"
-								body="In-progress app for users to manage payments. User registration and auth is done with Django and React-Redux."
-								filename={Payments}
-								link="https://payment-manager-test.herokuapp.com"
-							/>
-						</Grid>
-						<Grid className="project-grid-item" item xs={12} sm={6}>
-							<Project
-								title="Hiscore Tracker"
-								body="Site that pulls user data and profile image from an external API based on user search. Organizes data on front end to display."
-								filename={Woodcut}
-								link="https://woodcut.info/rs3/zee_pk"
-							/>
-						</Grid>
-					</Grid>
-				</div>
+
+				<ProjectGrid />
+
 				<Form />
 			</div>
 		);
---
+++ src/components/ProjectGrid.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Project from './Project';
+import Grid from '@material-ui/core/Grid';
+import EMT from '../images/podcast.png';
+import SuperPC from '../images/superpc.png';
+import Wildlife from '../images/wildlife.png';
+import Woodcut from '../images/woodcut2.png';
+import Arma from '../images/arma.png';
+import Payments from '../images/payments.png';
+import '../styles/ProjectGrid.css';
+
+export default class ProjectGrid extends Component {
+	render() {
+		return (
+			<div className="projects-container">
+				<Grid container spacing={3}>
+					<Grid className="project-grid-item" item xs={12} sm={6}>
+						<Project
+							title="Game Server Homepage"
+							body="Site for users to see news and updates for a video game server. Owner can update via Django Admin."
+							filename={Arma}
+							link="https://arma-homepage-demo.herokuapp.com/"
+						/>
+					</Grid>
+					<Grid className="project-grid-item" item xs={12} sm={6}>
+						<Project
+							title="Animal Crossing Wildlife"
+							body="Front End site for gamers to track achievements and see wildlife information. Saves progress using local storage."
+							filename={Wildlife}
+							link="https://www.acwildlife.com"
+						/>
+					</Grid>
+					<Grid className="project-grid-item" item xs={12} sm={6}>
+						<Project
+							title="Podcast Landing Page"
+							body="Landing site for information regarding an EMT podcast. Uses SVG for the background animation."
+							filename={EMT}
+							link="https://emtcast2.netlify.app/"
+						/>
+					</Grid>
+					<Grid className="project-grid-item" item xs={12} sm={6}>
+						<Project
+							title="Consultancy Support Site"
+							body="Business home page for a sample tech support company. Uses animations and parallax imagery for the background."
+							filename={SuperPC}
+							link="https://techsupportsite.netlify.app/"
+						/>
+					</Grid>
+					<Grid className="project-grid-item" item xs={12} sm={6}>
+						<Project
+							title="Payment Manager Web App"
+							body="In-progress app for users to manage payments. User registration and auth is done with Django and React-Redux."
+							filename={Payments}
+							link="https://payment-manager-test.herokuapp.com"
+						/>
+					</Grid>
+					<Grid className="project-grid-item" item xs={12} sm={6}>
+						<Project
+							title="Hiscore Tracker"
+							body="Site that pulls user data and profile image from an external API based on user search. Organizes data on front end to display."
+							filename={Woodcut}
+							link="https://woodcut.info/rs3/zee_pk"
+						/>
+					</Grid>
+				</Grid>
+			</div>
+		);
+	}
+}
