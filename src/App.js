b868261169d5cb5816a999f623197a571c23729a
Matt <matt@matthughes.dev>
2020-05-12 18:09:06 -0700

    tracking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Container from './components/Container.jsx';
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+	ReactGA.initialize('UA-160538981-1');
+	ReactGA.pageview('/homepage');
+}
+initializeReactGA();
 
 function App() {
 	return (
