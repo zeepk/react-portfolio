051cfd375d35df05c83cfcf01089c59be8964ff7
zeepk <zeepkdb@gmail.com>
2020-08-23 22:02:57 -0700

    colors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Container from './v2components/Container';
 import ReactGA from 'react-ga';
 import 'primeflex/primeflex.css';
 import Fade from 'react-reveal/Fade';
-
+// https://coolors.co/0e402d-295135-6b9075-e3dfff-731963
 function initializeReactGA() {
 	ReactGA.initialize('UA-160538981-1');
 	ReactGA.pageview('/homepage');
