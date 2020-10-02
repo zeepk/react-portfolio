5df50f52d0bea01f7acc9cf85e47d73d26dae870
Matt Hughes <zeepkdb@gmail.com>
2020-10-01 23:43:02 -0700

    removed unused components

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Container from './v2components/Container';
-import Uses from './v2components/Uses';
+import Container from './components/Container';
+import Uses from './components/Uses';
 
 export default function App() {
 	return (
